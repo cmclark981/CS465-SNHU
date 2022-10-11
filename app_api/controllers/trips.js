e7c156b137d88f8dcbdfa486cc2d48c15df99aee
Christopher Clark <cmclark981@gmail.com>
2022-10-11 17:03:20 -0500

    Module 7 adding security options.

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -1,10 +1,11 @@
 const { Model } = require('mongoose');
 const mongoose = require('mongoose');  //.set('debug', true);
-const model = mongoose.model('trips');
+const Trip = mongoose.model('trips');
+const User = mongoose.model('users');
 
 //list all trips
 const tripsList = async (req, res) => {
-    model
+    Trip
         .find({})  //empty filter for all trips
         .exec((err, trips) => {
             if (!trips) {
@@ -25,7 +26,7 @@ const tripsList = async (req, res) => {
 
 //Get: /trips/:tripCode - returns a single trip
 const tripsFindCode = async (req, res) => {
-    model   
+    Trip   
         .find({'code': req.params.tripCode})
         .exec((err, trip) => {
             if (!trip) {
@@ -45,7 +46,10 @@ const tripsFindCode = async (req, res) => {
 };
 
 const tripsAddTrip = async (req, res) => {
-    model
+    console.log('tripsAddTrip invoked with:\n' + req.body);
+    getUser(req, res,
+        (req, res) => {
+    Trip
         .create({
             code: req.body.code,
             name: req.body.name,
@@ -55,24 +59,27 @@ const tripsAddTrip = async (req, res) => {
             perPerson: req.body.perPerson,
             image: req.body.image,
             description: req.body.description
-    },
-    (err, trip) => {
-        if (err) {
-            return res
-                .status(400) //bad request, invalid content
-                .json(err);
-        } else {
-            return res
-                .status(201) //created
-                .json(trip);
+        },
+            (err, trip) => {
+                if (err) {
+                    return res
+                        .status(400) //bad request, invalid content
+                    .json(err);
+                } else {
+                    return res
+                        .status(201) //created
+                        .json(trip);
+                }       
+            });
         }
-               
-    });
+    )   
 }
 
 const tripsUpdateTrip = async (req, res) => {
     console.log(req.body);
-    model
+    getUser(req, res,
+        (req, res) => {
+    Trip
         .findOneAndUpdate({ 'code': req.params.tripCode }, {
             code: req.body.code,
             name: req.body.name,
@@ -103,9 +110,34 @@ const tripsUpdateTrip = async (req, res) => {
         return res
             .status(500) // server error
             .json(err);
-    });
+        });
+        }
+    )
 }
-       
+    
+const getUser = (req, res, callback) => {
+    if (req.payload && req.payload.email) {
+        User
+            .findOne({ email: req.payload.email })
+            .exec((err, user) => {
+                if (!user) {
+                    return res
+                        .status(404)
+                        .json({ "message": "User not found" });
+                } else if (err) {
+                    console.log(err);
+                    return res
+                        .status(404)
+                        .json(err);
+                }
+                callback(req, res, user.name);
+            });
+    } else {
+        return res
+            .status(404)
+            .json({ "message": "User not found" });
+    }
+};
 
 module.exports = {
     tripsList,
