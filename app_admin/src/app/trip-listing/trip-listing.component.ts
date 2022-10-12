d8c0ae4b2afe5ffd140ee691f38d14b7d143d13f
Christopher Clark <cmclark981@gmail.com>
2022-10-12 15:48:21 -0500

    Module 7 adding security options.

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 //import { trips } from '../data/trips';
-import { TripDataService } from 'services/trip-data.service';
-import { Trip } from 'models/trip';
+import { TripDataService } from 'src/app/services/trip-data.service';
+import { Trip } from 'src/app/models/trip';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-trip-listing',
@@ -19,6 +20,7 @@ export class TripListingComponent implements OnInit {
 
   constructor(
     private tripDataService: TripDataService,
+    private authService: AuthenticationService,
     private router: Router
     ) { }  //inject instance of service when class created
 
@@ -38,6 +40,10 @@ export class TripListingComponent implements OnInit {
         });
   }
 
+  public isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+  
   ngOnInit(): void {
     this.getTrips();  //invoke the local getTrips() when class initialized
   }
